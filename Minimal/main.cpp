7f83ab661956c571cca9e012004a05d5c106fee8
Luke S Rohrer <lrohrer@UCSD.EDU>
2018-06-12 01:45:35 -0700

    done

--- Minimal/main.cpp
+++ Minimal/main.cpp
@@ -495,7 +495,7 @@ public:
 		});
 		// Make the on screen window 1/4 the resolution of the render target
 		_mirrorSize = _renderTargetSize;
-		_mirrorSize /= 2;
+		_mirrorSize /= 4;
 	}
 
 protected:
